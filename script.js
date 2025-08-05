a9ab182bd99b1f852336ae7b1647c54f4585c5b4
The Way <mrs.kelly.uk@gmail.com>
2025-08-05 01:25:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-SQ302JW8XN"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-SQ302JW8XN'); </script>
